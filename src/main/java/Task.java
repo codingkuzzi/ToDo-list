77afc3fdbcf07a04c3e18d2d4a361f70f178e376
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-07-10 10:31:26 -0700

    add Category class and tests

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,46 +1,48 @@
-import java.util.ArrayList;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task {
-    private String mDescription;
-    private boolean mCompleted;
-    private LocalDateTime mCreatedAt;
-    private static ArrayList<Task> instances = new ArrayList<Task>();
-    private int mId;
+  private String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
+  private static List<Task> instances = new ArrayList<Task>();
+  private int mId;
 
-    public Task(String description) {
-        mDescription = description;
-        mCompleted = false;
-        mCreatedAt = LocalDateTime.now();
-        instances.add(this);
-        mId = instances.size();
-    }
+  public Task(String description) {
+    mDescription = description;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
+    mId = instances.size();
+  }
 
-    public String getDescription() {
-        return mDescription;
-    }
+  public String getDescription() {
+    return mDescription;
+  }
 
-    public boolean isCompleted() {
-        return mCompleted;
-    }
+  public boolean isCompleted() {
+    return mCompleted;
+  }
 
-    public LocalDateTime getCreatedAt() {
-        return mCreatedAt;
-    }
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
 
-    public static ArrayList<Task> all() {
-        return instances;
-    }
+  public static List<Task> all() {
+   return instances;
+ }
 
-    public static void clear() {
-        instances.clear();
-    }
+  public static void clear() {
+    instances.clear();
+  }
 
-    public int getId() {
-        return mId;
-    }
+  public int getId() {
+   return mId;
+ }
 
-    public static Task find(int id) {
-        return instances.get(id - 1);
-    }
+ public static Task find(int id) {
+   return instances.get(id - 1);
+ }
+
 }
